b14650bd98e508081d30e3b861ce65bc087b8c4a
kenjopa <kenjopa6@gmail.com>
2023-04-15 22:35:08 +0500

    fix styling

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
-<script setup lang="ts">
-import NavbarMain from "@/components/Navbar/NavbarMain.vue"
-import MainInfo from "@/components/Main/MainInfo.vue"
-</script>
 
 <template>
-  <div class="h-screen flex justify-center items-center bg-bg">
-    <div class="shadow-lg h-[600px] w-[900px] bg-white flex flex-row">
+  <div class="bg-main text-primary font h-screen flex justify-center items-center bg-bg">
+    <div class="shadow-lg h-[600px] w-[900px] bg-white flex flex-row rounded-xl">
       <nav class="bg-navbar bg-cover bg-no-repeat m-5 p-12 pr-24 rounded-lg text-white">
         <NavbarMain />
       </nav>
-      <main class="flex flex-col">
+      <main class="flex flex-col ml-10 mt-14 pr-10 max-w-lg break-words">
         <MainInfo />
       </main>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import NavbarMain from "@/components/Navbar/NavbarMain.vue"
+import MainInfo from "@/components/Main/MainInfo.vue"
+
+</script>
