d4f3afc9590505827c989d78f743fabdd7ade465
kenjopa <kenjopa6@gmail.com>
2023-04-15 21:52:19 +0500

    add variables in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: 'hsl(218, 100%, 97%)',
         primary: 'hsl(213, 96%, 18%)',
-        secondary: 'hsl(243, 100%, 62%)'
+        secondary: 'hsl(243, 100%, 62%)',
+        lightGrey: 'hsl(228, 100%, 83%)',
+        grey: 'hsl(231, 11%, 63%)'
       },
       backgroundImage: {
-        'navbar': "url('src/assets/images/sidebar-background.svg')"
+        navbar: "url('src/assets/images/sidebar-background.svg')",
+      },
+      backgroundColor: {
+        main: 'hsl(218, 100%, 97%)',
       }
     },
   },
