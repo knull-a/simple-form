057a7b4dbc9f5c29c02bacdf79571cc4f8f853de
kenjopa <kenjopa6@gmail.com>
2023-04-16 21:53:17 +0500

    add stores

--- src/components/Main/MainFooter.vue
+++ src/components/Main/MainFooter.vue
@@ -1,14 +1,18 @@
 <template>
-    <div class="flex justify-between mt-14">
-        <button v-show="!props.isFirstStep" @click.prevent class="text-grey font-bold hover:text-primary">Go back</button>
-        <button class="bg-primary text-white px-5 py-2 rounded-lg hover:brightness-125">Next Step</button>
+    <div class="absolute bottom-10">
+        <div class="flex justify-between mt-14">    
+            <button v-show="!props.isFirstStep" @click.prevent="$emit('goBack')" class="text-grey font-bold hover:text-primary">Go back</button>
+            <button class="bg-primary text-white px-5 py-2 rounded-lg hover:brightness-125">
+                <slot>Next Step</slot>
+            </button>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
 const props = defineProps<{
     isFirstStep: boolean
 }>()
-const emit = defineEmits<{
-    
+defineEmits<{
+   (e: 'goBack'): void
 }>()
 </script>
