861060aed7353a03ec7fa7836bef3713306039e6
kenjopa <kenjopa6@gmail.com>
2023-06-13 00:44:13 +0500

    add margin to button

--- src/components/Main/MainFooter.vue
+++ src/components/Main/MainFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="absolute bottom-10">
         <div class="flex justify-between mt-14">    
-            <button v-show="!props.isFirstStep" @click.prevent="$emit('goBack')" class="text-grey font-bold hover:text-primary">Go back</button>
+            <button v-show="!props.isFirstStep" @click.prevent="$emit('goBack')" class="text-grey font-bold hover:text-primary mr-64">Go back</button>
             <button class="bg-primary text-white px-5 py-2 rounded-lg hover:brightness-125">
                 <slot>Next Step</slot>
             </button>
